49fe470437bc59cd913fb490fe34b3b5a426bd7e
daryamakavchik <darjamakavchik@gmail.com>
2023-03-31 11:03:19 -0700

    feat: add ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/main',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'mestoproject'
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/daryamakavchik/mesto-project-plus.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+}; 
