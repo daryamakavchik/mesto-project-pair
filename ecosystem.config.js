6256303124d56bbbe05ddb18f2509275ce379191
daryamakavchik <darjamakavchik@gmail.com>
2023-03-31 11:29:19 -0700

    fix: ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/daryamakavchik/mesto-project-pair.git',
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm i && npm run build',
     },
   },
